677548ba91abf3c9332f84a59b9924d11e8d58f8
Pankaj Kumar <pankaj.kumar@codvo.ai>
2024-09-05 20:06:31 +0530

    Initial layout of HTML & CSS

---
+++ frontend/src/ChatApp/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import ChatContent from "./ChatContent/ChatContent";
+import ChatInputBox from "./ChatInputBox/ChatInputBox";
+import { useGetMessages } from "../hooks/useGetMessages";
+import { Message } from "../data";
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+
+const Chat = () => {
+  /** Simulate a hook fetching the data */
+  const {
+    messages: { data }
+  } = useGetMessages();
+
+  /** State to control new messages */
+  const [chatMessages, setChatMessages] = React.useState<Message[]>(data);
+
+  /**
+   *
+   * @param message
+   * "Create" a new message
+   */
+  const sendANewMessage = (message: Message) => {
+    setChatMessages((prevMessages) => [...prevMessages, message]);
+  };
+
+  /**
+   * Reset chat to the default messages
+   */
+  const resetChat = () => {
+    setChatMessages(data);
+  };
+
+  return (
+    // <div className="max-w-sm mx-auto my-auto">
+    //   <div className="bg-white border border-gray-200 rounded-lg shadow relative">
+    //     <ChatContent messages={chatMessages} />
+    //     <ChatInputBox sendANewMessage={sendANewMessage} />
+    //   </div>
+    // </div>
+
+
+<div className="h-screen flex items-center justify-center">
+  <div className="max-w-sm w-full bg-white border border-gray-200 rounded-lg shadow relative">
+    <ChatContent messages={chatMessages} />
+    <ChatInputBox sendANewMessage={sendANewMessage} />
+  </div>
+</div>
+
+  );
+};
+
+export default Chat;
